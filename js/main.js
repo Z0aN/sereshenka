32ee6a5146333d455b1ce6df5c7880d722b06b93
Z0aN <adrinal1ne30rus@mail.ru>
2022-08-22 13:25:31 +0400

    -create "AutoResizeTextarea + about"

--- js/main.js
+++ js/main.js
@@ -10,4 +10,21 @@ $(document).ready(function(){
         draggable: true,
         swipe: true,
     });
+
+    const txHeight = 30;
+    const tx = document.getElementsByTagName("textarea");
+
+    for (let i = 0; i < tx.length; i++) {
+    if (tx[i].value == '') {
+        tx[i].setAttribute("style", "height:" + txHeight + "px;overflow-y:hidden;");
+    } else {
+        tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+    }
+    tx[i].addEventListener("input", OnInput, false);
+    }
+
+    function OnInput(e) {
+    this.style.height = "auto";
+    this.style.height = (this.scrollHeight) + "px";
+    }
 });
