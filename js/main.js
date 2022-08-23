de7b27196011b801902ca3b749e499f74ffdfe6e
Z0aN <adrinal1ne30rus@mail.ru>
2022-08-23 21:00:02 +0400

    -fix "slider + add anim on button in contacts"

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         arrows:false,
         adaptiveHeight: true,
         slidesToShow : 3,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
         autoplay: true,
         centerMode: true,
         autoplaySpeed: 2000,
